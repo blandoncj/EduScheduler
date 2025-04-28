8fb5af3f64d389d3acb99765902f2befa68ee427
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-04-27 21:26:27 -0500

    feat(models): add classroom number to ClassroomModel
    Added a `number` attribute to the `ClassroomModel` class to represent
    the classroom number. Updated the constructor, `to_dict`, and
    `from_dict` methods to handle the new attribute.
    
    This change enhances the model by allowing classrooms to be
    distinguished by their number, improving data representation.

--- backend/app/models/classroom_model.py
+++ backend/app/models/classroom_model.py
@@ -2,13 +2,17 @@ import uuid
 
 
 class ClassroomModel:
-    def __init__(self, systems_room: bool = False, id: str = None):
+    def __init__(
+            self, number: int, systems_room: bool = False, id: str = None
+    ):
+        self.number = number
         self.systems_room = systems_room
         self.id = str(uuid.uuid4()) if id is None else id
 
     def to_dict(self):
         return {
             'id': self.id,
+            'number': self.number,
             'systems_room': self.systems_room
         }
 
@@ -16,5 +20,6 @@ class ClassroomModel:
     def from_dict(data):
         return ClassroomModel(
             id=data.get('id'),
+            number=data['number'],
             systems_room=data['systems_room']
         )
